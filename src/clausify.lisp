8ca997b83e901e5b2685f259e4f6a5ad93077c7b
let42 <alberto@gucciardi.xyz>
2018-02-19 15:30:46 +0100

    Added and tested the detenction functions and distributivity law, skolemization of const and fun, prototype of simplfy

--- src/clausify.lisp
+++ src/clausify.lisp
@@ -40,24 +40,37 @@
 ;;;; 10. (every ?y (exist ?x (p ?x ?y)) -> (every ?y (p sf<pr> ?y) ?y)
 
 ;;;; Funzioni di riconoscimento delle variabili costanti e dei simboli
+;;;; 1.(is-operator (opr))
+;;;; 2.(is-letter (iterm))
+;;;; 3.(is-symbol (iterm))
+;;;; 4.(is-variable (var))
+;;;; 5.(is-fun (iterm))
+;;;; 6.(is-const (iterm))
+;;;; 7.(is-term (iterm))
+;;;; 8.(is-literal (iform))
+;;;; 9.(arity (iform))
+;;;; 10.(is-wff (iform))
+;;;; Funzioni che implementano le regole di sostituzione
+;;;; 1.
+;;;; 2.
 
-;;;; Funzioni interfaccia:
-;;;;   1.(as-cnf (fbf)) -> riscrive la fbf in una formula cnf
-;;;;   2.(is-horn (fbf)) -> restituisce true se la fbf e' una clausola di Horn
+;;;; Funzioni interfaccia
+;;;; 1.(as-cnf (fbf)) -> riscrive la fbf in una formula cnf
+;;;; 2.(is-horn (fbf)) -> restituisce true se la fbf e' una clausola di Horn
 
 ;;;; Restituisce true se oper e' un operatore logico
-(defun is-an-operator (oper)
+(defun is-operator (opr)
   (or
-   (eq oper 'not)
-   (eq oper 'and)
-   (eq oper 'or)
-   (eq oper 'implies)
-   (eq oper 'every)
-   (eq oper 'exist)))
+   (eq opr 'not)
+   (eq opr 'and)
+   (eq opr 'or)
+   (eq opr 'implies)
+   (eq opr 'every)
+   (eq opr 'exist)))
 
 
 (defun is-letter (iterm)
-  (not (parse-integer (eval iterm) :start 0 :end 1 :junk-allowed t)))
+  (not (parse-integer (symbol-name iterm) :start 0 :end 1 :junk-allowed t)))
 
 ;;;; Restituisce true se iterm e' un simbolo che inizia con una lettera
 
@@ -72,11 +85,108 @@
    (not (listp var))
    (not (is-operator var))
    (symbolp var)
-   (char= #\? (char (eval var) 0))))
+   (char= #\? (char (symbol-name var) 0))))
+
+(defun is-fun (iterm)
+  (and
+   (not (null iterm))
+   (symbolp (first iterm))
+   (not (is-operator (first iterm)))
+   (every 'is-term (rest iterm))))
+
+;;;; Restituisce true se iterm contiene è una costante
+(defun is-const (iterm)
+  (and
+   (not (listp iterm))
+   (not (is-variable iterm))
+   (not (is-operator iterm))
+   (or
+    (numberp iterm)
+    (is-letter iterm))))
 
 ;;;; Restituisce true se iterm e' una costante, una variabile oppure una funzione
 (defun is-term (iterm)
 	(or
-		(constp iterm)
-		(variablep iterm)
-		(and (listp iterm) (funp iterm))))
+		(is-const iterm)
+		(is-variable iterm)
+		(and (listp iterm) (is-fun iterm))))
+
+;;;; Restituisce true se iform è un letterale
+(defun is-literal (iform)
+  (or
+   (is-term iform)
+   (and
+    (eq (first iform) 'not)
+    (is-term (second iform)))))
+
+
+;;;; Ritorna l'arità di una formula 
+(defun arity (iform)
+  (if (listp iform) (- (list-length iform) 1)))
+
+;;;; Restituisce true iform è una formula ben formata
+;;;; 
+(defun is-wff (iform)
+  (cond
+    ((is-term iform) T)
+
+    (T
+     (or
+      (and
+       (= (arity iform) 2)
+       (member (first iform) '(and or implies))
+       (is-wff (second iform))
+       (is-wff (third iform)))
+
+      (and
+       (= (arity iform) 2)
+       (member (first ifrom) '(exist every))
+       (is-variable (second iform))
+       (is-wff (third iform)))
+      (and
+       (= (arity iform) 1)
+       (member (first iform) '(not))
+       (is-wff (second iform)))))))
+
+;;;;
+
+(defun distributivity (iform)
+  (cond
+    ((not (listp iform)) iform)
+    ((and 
+      (eq (list-length iform) 3)
+      (listp (second iform))
+      (eq (first iform) 'or)
+      (eq (first (second iform)) 'and))
+     (list 'and
+	   (list 'or (dist (second (second iform))) (dist (third iform)))
+	   (list 'or (dist (third (second iform))) (dist (third iform)))))
+    ((and
+      (eq (list-length iform) 3)
+      (listp (third iform))
+      (eq (first iform) 'or)
+      (eq (first (third iform) 'and)))
+     (list 'and
+	   (list 'or (dist (second iform)) (dist (second (third iform))))
+	   (list 'or (dist (second iform)) (dist (third (third iform))))))
+    (T iform)))
+
+;;;; Functions for skolemization of constant and functions
+(defun skolem-vars ()
+  (gentemp "SV-"))
+
+(defun skolem-function* (&rest args)
+  (cons (gentemp "SF-") args))
+
+(defun skolem-function (args)
+  (apply #'skolem-function* args))
+
+
+;;;;
+(defun simplify (iform)
+  (cond
+    ((not (listp iform)) f)
+    (T
+     )))
+
+
